cc2e69901252db5b6a3e23125f5bb3a6a5d7a04b
tlaghzal <laghzaltarik0@gmail.com>
2026-03-11 10:15:20 +0100

    feat(time): implement precise millisecond timestamp utilities in coders/time.c and utils.c
    Implement high-precision timekeeping utilities using gettimeofday.
    
    - get_time_in_ms: returns monotonic millisecond timestamps since simulation start.
    - smart_sleep: prevents CPU spin while ensuring wakeups within 1 millisecond accuracy.

---
+++ coders/time.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 17:42:22 by tlaghzal          #+#    #+#             */
+/*   Updated: 2026/04/24 17:17:26 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+long long	get_time_in_ms(void)
+{
+	long long		current_time;
+	struct timeval	time_value;
+
+	if (gettimeofday(&time_value, NULL) == -1)
+		return (1);
+	current_time = (time_value.tv_sec * 1000) + (time_value.tv_usec / 1000);
+	return (current_time);
+}
+
+void	smart_sleep(long long time_to_sleep, t_all *all)
+{
+	long long	start_time;
+
+	start_time = get_time_in_ms();
+	while ((get_time_in_ms() - start_time) < time_to_sleep)
+	{
+		if (should_stop(all))
+			break ;
+		usleep(50);
+	}
+}
---
+++ coders/utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/14 23:05:32 by tlaghzal          #+#    #+#             */
+/*   Updated: 2026/05/13 12:54:02 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "codexion.h"
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+int	error_exit(const char *msg)
+{
+	write(2, msg, (size_t)ft_strlen(msg));
+	return (FAIL);
+}
+
+int	valdite_number(const char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	if (!s[i])
+		return (FAIL);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (FAIL);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+long long	ft_atol(const char *str)
+{
+	long long	res;
+	int			sign;
+	int			i;
+	int			digit;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] <= 13 && str[i] >= 9))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		digit = str[i] - '0';
+		if (res > (LLONG_MAX - digit) / 10)
+			return (LLONG_MAX);
+		res = res * 10 + digit;
+		i++;
+	}
+	return (res * sign);
+}
