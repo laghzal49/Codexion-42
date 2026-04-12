3973a74e0f258e26b5de71f6aa13f4fe3b38b196
tarik <laghzaltarik0@gmail.com>
2026-04-12 14:03:03 +0100

    Update coders/Makefile, coders/coder_routine.c, coders/codexion.c and 8 more

--- coders/monitor.c
+++ coders/monitor.c
@@ -1,7 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlaghzal <tlaghzal@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/11 15:42:57 by tlaghzal          #+#    #+#             */
+/*   Updated: 2026/04/11 18:44:00 by tlaghzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "codexion.h"
+#include <pthread.h>
+#include <unistd.h>
 
-
-int monitor()
+int	check_death(t_sim *sim, int i)
 {
+	long long	time_since_compl;
+
+	time_since_compl = get_time_in_ms() - sim->coders[i].last_compile;
+	if (time_since_compl >= sim->params.time_to_burnout)
+	{
+		pthread_mutex_lock(&sim->log_mutex);
+		printf("%lld %d burned out\n", 
+			get_time_in_ms() - sim->start_time_ms, sim->coders[i].id);
+		sim->stop_flag = 1;
+		pthread_mutex_unlock(&sim->log_mutex);
+		return (1);
+	}
 	return (0);
+}
+
+int	check_all_finished(t_sim *sim, int finished)
+{
+	if (sim->params.compiles_required > 0 
+		&& finished == sim->params.num_coders)
+	{
+		pthread_mutex_lock(&sim->stop_mutex);
+		sim->stop_flag = 1;
+		pthread_mutex_unlock(&sim->stop_mutex);
+		return (1);
+	}
+	return (0);
+}
+
+int	check_coders(t_sim *sim)
+{
+	int	i;
+	int	finished;
+
+	i = 0;
+	finished = 0;
+	while (i < sim->params.num_coders)
+	{
+		pthread_mutex_lock(&sim->stop_mutex);
+		if (check_death(sim, i))
+		{
+			pthread_mutex_unlock(&sim->stop_mutex);
+			return (1);
+		}
+		if (sim->params.compiles_required > 0 
+			&& sim->coders[i].compile_count >= sim->params.compiles_required)
+			finished++;
+		pthread_mutex_unlock(&sim->stop_mutex);
+		i++;
+	}
+	return (check_all_finished(sim, finished));
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_sim	*sim;
+
+	sim = (t_sim *)arg;
+	while (1)
+	{
+		if (check_coders(sim) == 1)
+			break ;
+		usleep(1000);
+	}
+	return (NULL);
 }
--- coders/simulation.c
+++ coders/simulation.c
@@ -6,7 +6,38 @@
 /*   By: tlaghzal <tlaghzal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 15:27:51 by tlaghzal          #+#    #+#             */
-/*   Updated: 2026/04/11 15:27:53 by tlaghzal         ###   ########.fr       */
+/*   Updated: 2026/04/11 16:09:18 by tlaghzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "codexion.h"
+#include <pthread.h>
+
+int start_sim(t_sim *sim)
+{
+  int   i;
+
+  i = 0;
+  sim->start_time_ms = get_time_in_ms();
+  while (i < sim->params.num_coders)
+  {
+    sim->coders[i].last_compile = sim->start_time_ms;
+    if (pthread_create(&sim->coders[i].thread, NULL, coder_routine, &sim->coders[i]) != 0)
+      return (1);
+    i++;
+  }
+  return (0);
+}
+
+void    join_sim(t_sim *sim)
+{
+  int   i;
+
+  i = 0;
+  while (i < sim->params.num_coders)
+  {
+    pthread_join(sim->coders[i].thread, NULL);
+    i++;
+  }
+}
 
